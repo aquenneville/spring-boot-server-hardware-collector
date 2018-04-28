84de0d0de8564223e4840447187d470d3c6dbc13
aquenneville <alainquenneville@gmail.com>
2018-04-28 21:39:43 +0100

    Update of the run-agent.sh

--- spring-server-hardware-collector-agent/run-agent.sh
+++ spring-server-hardware-collector-agent/run-agent.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-nohup java -jar spring-server-hardware-collector-agent-1.0-SNAPSHOT.jar --server.hardware.collector.port=9390 --server.hardware.collector.ip=192.168.0.215 
+nohup java -jar spring-server-hardware-collector-agent-1.0-SNAPSHOT.jar --server.hardware.collector.port=9390 --server.hardware.collector.ip=x.x.x.x
