ecbc754d2f0f4da15983cc6753495eeabe70134d
aquenneville <alainquenneville@gmail.com>
2018-04-29 23:52:28 +0100

    Update run-agent.sh

--- spring-server-hardware-collector-agent/run-agent.sh
+++ spring-server-hardware-collector-agent/run-agent.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
 nohup java -jar spring-server-hardware-collector-agent-1.0-SNAPSHOT.jar --server.hardware.collector.port=9390 --server.hardware.collector.ip=x.x.x.x
+curl -v http://localhost:8081/api/send-config
