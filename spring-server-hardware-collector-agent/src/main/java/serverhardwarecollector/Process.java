5783d26465c4ceadc16bef4633e6cdefcaeaf679
aquenneville <alainquenneville@gmail.com>
2018-08-22 23:31:34 +0100

    Add handling of illegal state exception

--- spring-server-hardware-collector-agent/src/main/java/serverhardwarecollector/Process.java
+++ spring-server-hardware-collector-agent/src/main/java/serverhardwarecollector/Process.java
@@ -7,9 +7,15 @@ import org.apache.commons.exec.CommandLine;
 import org.apache.commons.exec.DefaultExecutor;
 import org.apache.commons.exec.ExecuteException;
 import org.apache.commons.exec.PumpStreamHandler;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import serverhardwarecollector.exception.IllegalApplicationStateException;
 
 public class Process {
 
+	private static final Logger logger = LoggerFactory.getLogger(Process.class);
+	
 	public static String collectExternalProcessData(String command) {
 		ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
 		String line = command;
@@ -21,11 +27,11 @@ public class Process {
 		try {
 			executor.execute(cmdLine);
 		} catch (ExecuteException e) {
-			System.out.println("Something went wrong during the execution of the command.");
-			return("hardware discovery failed");
+			logger.error(e.getMessage());
+			throw new IllegalApplicationStateException("Error: hardware collection of the data could not complete.");
 		} catch (IOException e) {
-			System.out.println("Something went wrong during the IO execution of the command.");
-			return("hardware discovery failed");
+			logger.error(e.getMessage());
+			throw new IllegalApplicationStateException("Error: hardware collection of the data could not complete.");
 		} 		
 		return(outputStream.toString().replaceAll("[\\r\\n]", ""));
 	}
